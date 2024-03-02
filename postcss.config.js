314a3d7fc625e598f1bbf3546700fb5afe46170c
nakagawa333 <naka33321@gmail.com>
2024-03-02 21:06:55 +0900

    PWA 初期対応

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  })
+
+module.exports = withPWA({
+    //next.js config
+    reactStrictMode: false,
+})
