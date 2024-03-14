072e58e256cff3f30968afc3b6adf7ffade6f4e4
nakagawa333 <naka33321@gmail.com>
2024-03-15 01:40:52 +0900

    スケジュール変更 デザイン デグレ対応

--- src/app/components/SchedulerModal.tsx
+++ src/app/components/SchedulerModal.tsx
@@ -168,7 +168,7 @@ export const SchedulerModal = (props:Props) => {
                className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex"
                tabIndex={tabIndex}
                style={{ 
-                    background: "rgba(200,200,200)", 
+                    background: "rgba(200,200,200,0.5)", 
                 }}  
             >
                 <div 
